91b6dd54d22a80aefb86f80860e254e72397b902
ayushtiwari57 <ayushtiwari572000@gmail.com>
2019-05-29 09:54:52 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Initialize and add the map
+function initMap() {
+    // Your location
+    const loc = { lat: 28.620764, lng: 77.363930 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+
+
+
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
